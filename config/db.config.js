4f3a6bafc8488e86a1ec5513e0dd9c84271e0c3d
ayudha <ayudharachman@gmail.com>
2024-08-07 16:04:45 +0700

    add

---
+++ config/db.config.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'product'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL Database!');
+});
+
+module.exports = connection;
